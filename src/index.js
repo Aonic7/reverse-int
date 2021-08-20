cd9930639854993e8f18f7d7d7df6c14e4969e2d
aonic7 <arsenal.rvp41@gmail.com>
2021-08-20 23:03:02 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let n1 = Math.abs(n);
+    const arr = Array.from(n1.toString()).map(Number);
+    let arrrev = [];
+
+    for (let i = 0; i < arr.length; i++) {
+
+        arrrev[arr.length - 1 - i] = arr[i];
+    }
+
+    const num = Number(arrrev.join(''));
+    return num;
 }
